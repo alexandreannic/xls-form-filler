afa0a1930e8ed4513615118c7bb5b404a5c63018
Alexandre Annic <alexandre.annic1@gmail.com>
2025-06-04 15:36:07 -0500

    ok

--- src/XlsFormFiller/index.ts
+++ src/XlsFormFiller/index.ts
@@ -1,2 +1,6 @@
 export type {XlsFormProps, XlsFormFillerHandle} from './XlsFormFiller.tsx'
 export {XlsFormFiller} from './XlsFormFiller.tsx'
+// git remote set-url origin https://alexandreannic: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@github.com/alexandreannic/xls-form-filler.git
+
+// git remote set-url origin git@github.com:alexandreannic/xls-form-filler.git
+
